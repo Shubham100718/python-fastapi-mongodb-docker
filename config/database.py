4317f8df4b5135344e1e9101d77fe0dce54bc4a0
Shubham Saxena <saxenashubham2207@gmail.com>
2024-03-22 00:15:51 +0530

    update

--- config/database.py
+++ config/database.py
@@ -2,10 +2,10 @@ from pymongo import MongoClient
 
 
 # client = MongoClient("mongodb+srv://prince:<password>@cluster0.xfinu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
-client = MongoClient("mongodb://localhost:27017/mydb")
+# client = MongoClient("mongodb://localhost:27017/mydb")
 # client = MongoClient(f"mongodb://{USERNAME}:{PASSWORD}@{MONGODB_HOST}:{MONGODB_PORT}/")
+client = MongoClient(f"mongodb://user:pass@mongo:27017/my_db")
 
 db = client.todo_app
 
 collection_name = db["todos_app"]
-
--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import uvicorn
 from fastapi import FastAPI
 from routes.todos_route import todo_api_router
 
 app = FastAPI()
 
 app.include_router(todo_api_router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, reload=True)
 
